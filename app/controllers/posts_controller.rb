4ce46648b9935a2f807bd1613afb019f0b83f681
Daniduran-dev <danigduran@gmail.com>
2021-03-12 22:36:01 +0700

    create before_action and posts_controller#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
+  before_action :user_signed_in?, only: %i[new create]
   def new
+    @post = Post.new
   end
 
-  def create
-  end
+  def create; end
 
-  def index
-  end
+  def index; end
 end
